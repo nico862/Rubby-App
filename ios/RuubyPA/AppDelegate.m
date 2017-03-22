22e1d69e4cfc4aaaebde5185853cdaed4178bdb8
Nicholas <nicowork862@gmail.com>
2017-03-22 13:02:25 +0100

    react-native version bump

--- ios/RuubyPA/AppDelegate.m
+++ ios/RuubyPA/AppDelegate.m
@@ -9,10 +9,8 @@
 
 #import "AppDelegate.h"
 
-// The following line for production
-#import "RCTBundleURLProvider.h"
-#import "RCCManager.h"
-#import "RCTRootView.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
@@ -20,40 +18,19 @@
 {
   NSURL *jsCodeLocation;
 
-#if DEBUG
-#if TARGET_OS_SIMULATOR
-  #warning "DEBUG SIMULATOR"
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/build/index.ios.bundle?platform=ios&dev=true"];
-#else
-  #warning "DEBUG DEVICE"
-  NSString *serverIP = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"SERVER_IP"];
-  NSString *jsCodeUrlString = [NSString stringWithFormat:@"http://%@:8081/build/index.ios.bundle?platform=ios&dev=true", serverIP];
-  NSString *jsBundleUrlString = [jsCodeUrlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-  jsCodeLocation = [NSURL URLWithString:jsBundleUrlString];
-#endif
-#else
-  #warning "PRODUCTION DEVICE"
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
-  // **********************************************
-  // *** DON'T MISS: THIS IS HOW WE BOOTSTRAP *****
-  // **********************************************
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"RuubyPA"
+                                               initialProperties:nil
+                                                   launchOptions:launchOptions];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  self.window.backgroundColor = [UIColor whiteColor];
-  [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation];
-
-  // RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-  //                                                     moduleName:@"RuubyPA"
-  //                                              initialProperties:nil
-  //                                                  launchOptions:launchOptions];
-  // rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
-  // self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  // UIViewController *rootViewController = [UIViewController new];
-  // rootViewController.view = rootView;
-  // self.window.rootViewController = rootViewController;
-  // [self.window makeKeyAndVisible];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
   return YES;
 }
 
